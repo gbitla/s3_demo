5423a69a7358d21e73b0ade41a48779fcca5731a
Girish Bitla <gbitla@salesforce.com>
2018-07-03 22:58:11 -0700

    finally able to get s3 backend working

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "gb1-terraform-up-and-running-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
