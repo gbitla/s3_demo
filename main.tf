c0de2603e6da27fba1db7eb12ea182c7efe3843b
Girish Bitla <gbitla@salesforce.com>
2018-07-03 23:34:50 -0700

    updated remote state configuration

--- main.tf
+++ main.tf
@@ -14,10 +14,31 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+#reference - http://2ndwatch.com/blog/migratingterraformremoate/
 terraform {
   backend "s3" {
-    bucket = "gb1-terraform-up-and-running-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "gb1-terraform-up-and-running-state"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
+
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+output "s3_bucket_id" {
+  value = "${aws_s3_bucket.terraform_state.id}"
+}
+
+#data "terraform_remote_state" "foo" {
+#  backend = "s3"
+#
+#  config {
+#    bucket = "gb1-terraform-up-and-running-state"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
+
